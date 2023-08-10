63cc2525d58bf782ed585fda0ed8f3bc63260a51
Altyw <koshokov996@gmail.com>
2023-08-10 21:22:34 +0600

    reset password

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,6 +4,7 @@ import MainLayouts from "../layouts/MainLayouts";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
 import ActivationPage from "../pages/ActivationPage";
+import ResetPassPage from "../pages/ResetPassPage";
 
 const MainRoutes = () => {
   return (
@@ -13,6 +14,10 @@ const MainRoutes = () => {
       </Route>
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/api/account/activate/" element={<ActivationPage />} />
+      <Route
+        path="/api/account/reset-password/confirm/"
+        element={<ResetPassPage />}
+      />
     </Routes>
   );
 };
