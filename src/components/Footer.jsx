b59c594bcd9e6066b7815c5f3660e43a65b11f64
Gulnur <gulnurKalmatova123@gmail.com>
2023-08-10 11:23:57 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="main-footer">
+      <div className="container" style={{ marginTop: "30px" }}>
+        <div className="row">
+          <div className="col">
+            <h3>DESTINATIONS</h3>
+            <ul className="list-unstyled">
+              <li>Osh</li>
+              <li>Issyk-kul</li>
+              <li>Narun</li>
+              <li>Chui</li>
+              <li>Talas</li>
+              <li>Batken</li>
+              <li>Jalal-Abad</li>
+            </ul>
+          </div>
+          <div className="col">
+            <h3>CONTACTS</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
