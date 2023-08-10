3ecc57eae3fbdce047e2f9fa921aa3d338065c8b
Jadurion <jaddurion@gmail.com>
2023-08-10 19:21:48 +0600

    aboutus route

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -4,12 +4,14 @@ import MainLayouts from "../layouts/MainLayouts";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
 import ActivationPage from "../pages/ActivationPage";
+import AboutUsPage from "../pages/AboutUsPage";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route element={<MainLayouts />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutUsPage />} />
       </Route>
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/api/account/activate/" element={<ActivationPage />} />
